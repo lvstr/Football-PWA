5997ef741440f30d7d4b36b4dc0fb1a8d884e3c6
lvstr <serankucing@gmail.com>
2020-11-08 16:54:15 +0700

    update

--- index.js
+++
@@ -1,3 +0,0 @@
-import "./script/main.js";
-import "./script/register-service-worker.js";
-import "./script/push/register-notification.js";
--- script/module/detail-module.js
+++
@@ -1,5 +0,0 @@
-import "../detail-team-main.js";
-import "../db/idb.js";
-import "../api/api-listener.js";
-import "../register-service-worker.js";
-import "../push/register-notification.js";
